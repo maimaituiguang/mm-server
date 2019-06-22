65ad561fa5338ff68b4bed2acb8ccd1cd46cf6b9
maimaituiguang <1107821214@qq.com>
2019-06-22 13:15:50 +0800

    add file

--- server/adminQuery.py
+++ server/adminQuery.py
@@ -41,13 +41,8 @@ def update_role(role, phone):
         re = account.find_one({'phone': int(phone)})
         re = account.find_one({'user_id': re['yao_code']})
 
-        ms = conn.db['member'].find({})
-        price = 0
-        for m in ms:
-            if role != m['type']:
-                continue
-            price = m['price'] * 5.0 / 100.0
-            break
+        ms = conn.db['member'].find_one({'type': int(role)})
+        price = ms['price'] * 5.0 / 100.0
 
         conn.db['yao_record'].insert_one({'phone': int(re['phone']), 'yao_phone': int(phone), 'price': price, 'create_time': time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())})
         conn.db['wallet'].wallet.update_one({'phone': int(re['phone'])}, {'$inc': {'un_take': price}})
