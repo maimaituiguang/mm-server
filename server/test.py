fe05258258b689d732345441364784c4e0c0eb03
maimaituiguang <1107821214@qq.com>
2019-07-18 14:08:14 +0800

    add file

---
+++ server/test.py
@@ -0,0 +1,18 @@
+# -*- coding: UTF-8 -*-
+
+import conn
+import time
+import datetime
+
+
+def run():
+    yao = conn.db['yao_record']
+    re = yao.find({})
+    # print(len(list(re)))
+    for item in re:
+        stamp = time.mktime(datetime.datetime.strptime(item['create_time'], "%Y-%m-%d %H:%M:%S").timetuple())
+        yao.update_one({'_id': item['_id']}, {'$set': {'create_timestamp': int(stamp)}})
+        print(stamp)
+
+if __name__ == '__main__':
+    # run()
