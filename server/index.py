b2d6a8f11b992cd1d3eb370de45b298b80c12a75
maimai <1107821214@qq.com>
2019-08-04 11:55:06 +0800

    add file

--- server/index.py
+++ server/index.py
@@ -102,7 +102,7 @@ def message():
 
 @app.route('/banner')
 def banner():
-    data = ['https://maimaituiguang.github.io/mm-web/images/banner4.png']
+    data = ['https://maimaituiguang.github.io/mm-web/images/banner1.png', 'https://maimaituiguang.github.io/mm-web/images/banner4.png']
     return __response(json.dumps({'data': data}))
 
 
