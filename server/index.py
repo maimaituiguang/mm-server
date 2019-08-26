885317941b4a4ad426af8de9ec598e0e041810e5
taffy <taffy@zhihu.com>
2019-08-26 14:36:23 +0800

    add file

--- server/index.py
+++ server/index.py
@@ -107,7 +107,7 @@ def message():
 
 @app.route('/banner')
 def banner():
-    data = ['https://maimaituiguang.github.io/mm-web/images/banner6.png',
+    data = ['https://maimaituiguang.github.io/mm-web/images/banner7.png',
         'https://maimaituiguang.github.io/mm-web/images/banner1.png']
     return __response(json.dumps({'data': data}))
 
